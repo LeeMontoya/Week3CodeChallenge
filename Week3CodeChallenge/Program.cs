d45b40c8581df588d1fd261ed47e2a854115bb9f
LeeMontoya <jleemontoya@gmail.com>
2014-04-25 16:53:45 -0600

    Check in 2

---
+++ Week3CodeChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FindManyPrimes(10001);
+            Console.ReadKey();
+            
+        }
+
+        static void FindManyPrimes(int thisMany)
+        {
+            int curNum = 2;
+            int numPrimes = 0;
+
+            while (numPrimes < thisMany)
+            {
+                if (IsPrime(curNum))
+                {
+                    numPrimes += 1;
+                }
+
+                curNum += 1;
+            }
+
+            Console.WriteLine(curNum);
+        }
+
+        public static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+			{
+                if (number % i == 0)
+                {
+                    return false;
+                }
+
+            }
+            {
+                return true;
+            }
+        }
+        
+
+     }
+}
